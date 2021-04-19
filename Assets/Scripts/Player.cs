a4b5b88dc32c1a2ef5590cb46b232ad7d240ae50
devwearsprada <borissmeenk@protonmail.com>
2021-04-19 14:23:38 +0200

    Command, TargetRPC, ClientRPC and SyncVar examples

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
+    [SyncVar(hook = nameof(OnHelloCountChanged))] 
+    int helloCount = 0;
     void HandleMovement()
     {
         if (isLocalPlayer)
@@ -19,5 +21,38 @@ public class Player : NetworkBehaviour
     void Update()
     {
         HandleMovement();
+
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Sending to the server...");
+            Hello();
+        }
+
+        if (isServer && transform.position.y > 50)
+        {
+            TooHigh();
+        }
+    }
+
+    [Command] void Hello()
+    {
+        Debug.Log("Received hello from client");
+        helloCount += 1;
+        ReplyHello();
+    }
+
+    [TargetRpc] void ReplyHello()
+    {
+        Debug.Log("Received hello from server");
+    }
+
+    [ClientRpc] void TooHigh()
+    {
+        Debug.Log("Too high");
+    }
+
+    void OnHelloCountChanged(int oldCount, int newCount)
+    {
+        Debug.Log($"We had {oldCount}, but now we have {newCount}");
     }
 }
---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"/Applications/Unity/Hub/Editor/2020.3.3f1/Unity.app/Contents/MacOS/Unity" -quit -batchmode -projectPath "/Users/borissmeenk/Networking" -executeMethod BuildScript.BuildAll
+echo "Finished build!"
